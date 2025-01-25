be48d746efe73b0d3c0923a283c54634a81c6d38
Volodymyr <yushkevich.v2002@gmail.com>
2025-01-25 23:10:45 +0100

    Create and modified TollStation.java file in path model

---
+++ src/main/java/pyv/model/TollStation.java
@@ -0,0 +1,31 @@
+package pyv.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TollStation {
+    private String name;
+    private String city;
+    private int totalToll;
+    private List<Vehicle> vehicles;
+
+    public TollStation(String name, String city) {
+        this.name = name;
+        this.city = city;
+        this.totalToll = 0;
+        this.vehicles = new ArrayList<>();
+    }
+
+    public void registerVehicle(Vehicle vehicle) {
+        vehicles.add(vehicle);
+        totalToll += vehicle.calculateToll();
+    }
+
+    public int getTotalToll() {
+        return totalToll;
+    }
+
+    public List<Vehicle> getVehicles() {
+        return vehicles;
+    }
+}
