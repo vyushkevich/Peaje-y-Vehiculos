15627aad9f29c0cad700bb4d16d8a5b82af6b8f1
Volodymyr <yushkevich.v2002@gmail.com>
2025-01-25 22:51:20 +0100

    Create path model with file Vehicle.java, modified

---
+++ src/main/java/pyv/model/Vehicle.java
@@ -0,0 +1,15 @@
+package pyv.model;
+
+public abstract class Vehicle {
+    private String licensePlate;
+
+    public Vehicle(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public abstract int calculateToll();
+}
